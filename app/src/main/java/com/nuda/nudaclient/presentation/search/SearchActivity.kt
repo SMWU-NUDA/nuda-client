82a247ff6a116f02c294194ae1d04b57ee45daf8
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-03-20 14:46:36 +0900

    feat(search): 상품, 성분 검색 버튼에 따른 UI 업데이트
    - 상품, 성분 검색 버튼 클릭에 따른 화면 모드 변경 및 UI 업데이트 로직 구현

--- app/src/main/java/com/nuda/nudaclient/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/nuda/nudaclient/presentation/search/SearchActivity.kt
@@ -10,7 +10,7 @@ import com.nuda.nudaclient.presentation.common.activity.BaseActivity
 
 class SearchActivity : BaseActivity() {
 
-    // TODO 제품, 상품 검색 버튼 클릭에 따라 UI 업데이트 - 화면 상태 변수 필요!
+    // TODO 제품, 상품 검색 버튼 클릭에 따라 UI 업데이트
     // TODO 제품 검색일 떄, 상품 검색일 때 나눠서 처리 필요 (API 호출도 따로 관리)
     // TODO 제품 검색 결과, 상품 검색 결과 둘 다 같은 액티비티지만 intent에 상태 변수 담아서 함께 전달
 
@@ -30,6 +30,10 @@ class SearchActivity : BaseActivity() {
         }
         
         setToolbar() // 툴바 설정
+
+        setSearchMode() // 화면 모드 설정
+
+
     }
 
     // 툴바 설정
@@ -40,4 +44,43 @@ class SearchActivity : BaseActivity() {
         setToolbarShadow(false) // 툴바 그림자 제거
     }
 
+    // 화면 모드에 따른 UI 업데이트 (인기 검색어 API 호출)
+    private fun setUIByMode(pageMode: String) {
+        when(pageMode) {
+            "product" -> {
+                
+            }
+            "ingredient" -> {
+
+            }
+        }
+    }
+
+
+    // 모드 선택 버튼
+    private fun setSearchMode() {
+        // (기본값) 제품 검색
+        setSearchModeButton(true)
+        setUIByMode("product")
+
+        // 제품 검색 버튼
+        binding.btnSearchProduct.setOnClickListener {
+            setSearchModeButton(true)
+            setUIByMode("product")
+        }
+        // 상품 검색 버튼
+        binding.btnSearchIngredient.setOnClickListener {
+            setSearchModeButton(false)
+            setUIByMode("ingredient")
+        }
+    }
+
+    // 버튼 모드 설정
+    private fun setSearchModeButton(isProductMode: Boolean) {
+        binding.btnSearchProduct.isSelected = isProductMode
+        binding.btnSearchIngredient.isSelected = !isProductMode
+    }
+
+    // 검색어 자동 완성
+    // TODO 검색어 자동 완성
 }
