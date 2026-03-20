15d346ebd91c8d1820be4c496ec1759a35defe9f
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-03-20 15:46:15 +0900

    feat(search): 상품, 성분 인기 검색어 조회 연동
    - 상품, 성분 검색에 따른 인기 검색어 조회 API 연동 및 UI 업데이트

--- app/src/main/java/com/nuda/nudaclient/data/remote/dto/search/SearchCommonResponse.kt
+++ app/src/main/java/com/nuda/nudaclient/data/remote/dto/search/SearchCommonResponse.kt
@@ -1,5 +1,5 @@
 package com.nuda.nudaclient.data.remote.dto.search
 
 data class SearchCommonResponse(
-    val top10: List<String>
+    val stringList: List<String>
 )
--- app/src/main/java/com/nuda/nudaclient/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/nuda/nudaclient/presentation/search/SearchActivity.kt
@@ -1,11 +1,14 @@
 package com.nuda.nudaclient.presentation.search
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.nuda.nudaclient.R
+import com.nuda.nudaclient.data.remote.RetrofitClient.searchService
 import com.nuda.nudaclient.databinding.ActivitySearchBinding
+import com.nuda.nudaclient.extensions.executeWithHandler
 import com.nuda.nudaclient.presentation.common.activity.BaseActivity
 
 class SearchActivity : BaseActivity() {
@@ -15,6 +18,21 @@ class SearchActivity : BaseActivity() {
 
     private lateinit var binding : ActivitySearchBinding
 
+    // 인기 검색어 리스트 객체 생성
+    private val popular_keywords by lazy {
+        listOf(
+            binding.tvSearch,
+            binding.tvSearch2,
+            binding.tvSearch3,
+            binding.tvSearch4,
+            binding.tvSearch5,
+            binding.tvSearch6,
+            binding.tvSearch7,
+            binding.tvSearch8,
+            binding.tvSearch9,
+            binding.tvSearch10
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -43,20 +61,8 @@ class SearchActivity : BaseActivity() {
         setToolbarShadow(false) // 툴바 그림자 제거
     }
 
-    // 화면 모드에 따른 UI 업데이트 (인기 검색어 API 호출)
-    private fun setUIByMode(pageMode: String) {
-        when(pageMode) {
-            "product" -> {
-                
-            }
-            "ingredient" -> {
 
-            }
-        }
-    }
-
-
-    // 모드 선택 버튼
+    // 모드 선택 버튼 설정
     private fun setSearchMode() {
         // (기본값) 제품 검색
         setSearchModeButton(true)
@@ -75,9 +81,53 @@ class SearchActivity : BaseActivity() {
     }
 
     // 버튼 모드 설정
-    private fun setSearchModeButton(isProductMode: Boolean) {
+    private fun setSearchModeButton(isProductMode: Boolean) { // 둘 중 하나만 선택 가능하도록
         binding.btnSearchProduct.isSelected = isProductMode
         binding.btnSearchIngredient.isSelected = !isProductMode
+    }
+
+    // 화면 모드에 따른 UI 업데이트 (인기 검색어 API 호출)
+    private fun setUIByMode(pageMode: String) {
+        when(pageMode) {
+            "product" -> { // 상품 인기 검색어 조회 API 호출
+                searchService.getProductTOP10()
+                    .executeWithHandler(
+                        context = this,
+                        onSuccess = { body ->
+                            if (body.success == true) {
+                                body.data?.let { data ->
+                                    setPopularKeywords(data.stringList)
+                                }
+                            }
+                        }
+                    )
+            }
+            "ingredient" -> { // 성분 인기 검색어 조회 API 호출
+                searchService.getIngredientTOP10()
+                    .executeWithHandler(
+                        context = this,
+                        onSuccess = { body ->
+                            if (body.success == true) {
+                                body.data?.let { data ->
+                                    setPopularKeywords(data.stringList)
+                                }
+                            }
+                        }
+                    )
+            }
+        }
+    }
+
+    // 인기 검색어 업데이트
+    private fun setPopularKeywords(keywords: List<String>) {
+        popular_keywords.forEachIndexed { index, textView ->
+            if (index < keywords.size) {
+                textView.visibility = View.VISIBLE
+                textView.text = "${index + 1} ${keywords[index]}"
+            } else {
+                textView.visibility = View.GONE
+            }
+        }
     }
 
     // 검색어 자동 완성
